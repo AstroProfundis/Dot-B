7848c5125ddac5575cadccf43f4e8a8f83015431
Allen Zhong <allen@moe.cat>
2023-02-15 23:00:18 +0800

    sync custom footer change

--- footer.php
+++ footer.php
@@ -6,9 +6,9 @@ $dotb_settings = get_option( 'dotb_options', $dotb_options ); ?>
 				<?php echo $dotb_settings['dotb_footer'];?>
 			</div>
 			<div id="site-generator">
-				Powered by <a href="http://wordpress.org/">WordPress</a>
-				 | Theme <abbr title="Dot-B v<?php echo $dotb_settings['dotb_version'];?>">Dot-B</abbr> by <a href="http://zlz.im/" >hzlzh</a> <?php if ($dotb_settings['dotb_is_sqlcount']) 
-				{ echo '{ '.get_num_queries().' '.__('queries in', 'dot-b').' '.timer_stop(0,3).' '.__('seconds', 'dot-b').' }';}?>
+				本馆采用 <a href="http://wordpress.org/" target="_blank">WordPress</a> 强力驱动
+				 | 精美主题 <abbr title="Dot-B v<?php echo $dotb_settings['dotb_version'];?>" target="_blank">Dot-B</abbr> 由 <a href="http://zlz.im/" target="_blank">hzlzh</a> 制作 | <del>粤ICP备</del>菇ICP备060680
+				<?php if ($dotb_settings['dotb_is_sqlcount']){ echo '{ '.get_num_queries().' '.__('queries in', 'dot-b').' '.timer_stop(0,3).' '.__('seconds', 'dot-b').' }';}?>
 			</div>
 		</div><!-- #copyright -->
 		<a id="return_top" href="#wrapper" rel="nofollow" title="<?php _e('Back to top', 'dot-b'); ?>"> &Delta;  <?php _e('Top', 'dot-b'); ?></a>
